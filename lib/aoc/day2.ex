29fe2fdd3aab557c378836e9d748f1825aad855b
Cory Schmitt <cory@taxjar.com>
2018-12-01 23:24:28 -0800

    day 2, part 2, needs so much refactoring

--- lib/aoc/day2.ex
+++ lib/aoc/day2.ex
@@ -13,6 +13,33 @@ defmodule Aoc.Day2 do
     twos * threes
   end
 
+  def find_box do
+    box_ids =
+      File.stream!("priv/day2.txt")
+      |> Enum.map(&String.trim/1)
+
+    Enum.reduce_while(box_ids, nil, fn id, _acc ->
+      box_search =
+        Enum.reduce_while(box_ids, nil, fn bid, _acc ->
+          md = String.myers_difference(bid, id)
+          if bid != id and length(md) == 4 and String.length(Keyword.get(md, :del)) == 1 do
+            correct_box =
+              Keyword.get_values(md, :eq)
+              |> Enum.join()
+
+            {:halt, correct_box}
+          else
+            {:cont, nil}
+          end
+        end)
+
+      if box_search == nil do
+        {:cont, false}
+      else
+        {:halt, box_search}
+      end
+    end)
+  end
 
   defp count_letters(characters) do
     letter_counts =
